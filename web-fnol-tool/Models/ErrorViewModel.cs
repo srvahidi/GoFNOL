307acf6a04bb318478fc7328445e7791a2babc1e
Sam Ouimette <sam.ouimette@autopoint.com>
2017-12-11 14:40:57 -0700

    Initial commit
    Signed-off-by: Anthony Williams <anthony.williams@audatex.com>

---
+++ web-fnol-tool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_fnol_tool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

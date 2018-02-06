b623a613e54a1d96130af439fd3c2ae854f011c9
Sam Ouimette <sam.ouimette@autopoint.com>
2018-02-06 10:03:08 -0700

    cicd pipeline for int env [#154942096]
    Signed-off-by: Dmytro Sviridov <dmytro.sviridov@autopoint.com>

---
+++ ci/build-pipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+cp pipeline-resources.yml merged-pipeline.yml
+echo -e "\n" >> merged-pipeline.yml
+cat pipeline-resources-template.yml | sed -e "s/\[ENV\]/devtest/" >> merged-pipeline.yml
+echo -e "\n">> merged-pipeline.yml
+cat pipeline-resources-template.yml | sed -e "s/\[ENV\]/int/" >> merged-pipeline.yml
+echo -e "\n\njobs:\n" >> merged-pipeline.yml
+cat pipeline-jobs.yml >> merged-pipeline.yml
+echo -e "\n">> merged-pipeline.yml
+cat pipeline-jobs-template.yml | sed -e "s/\[ENV\]/devtest/" >> merged-pipeline.yml
+echo -e "\n">> merged-pipeline.yml
+cat pipeline-jobs-template.yml | sed -e "s/\[ENV\]/int/" >> merged-pipeline.yml
+
+echo "merged pipeline file created successfully"
+echo "run 'fly -t [target] sp -p GoFNOL -c merged-pipeline.yml' to update pipeline"

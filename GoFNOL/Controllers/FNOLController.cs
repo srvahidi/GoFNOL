0c506dddb4bc0dbaf87c5cbd4de85e89c641bc34
Williams, Anthony (Global Data & Content) <Anthony.Williams@audatex.com>
2018-07-12 14:11:24 -0600

    Display env in GoFNOL and fix lowercase o [#158944094]

--- GoFNOL/Controllers/FNOLController.cs
+++ GoFNOL/Controllers/FNOLController.cs
@@ -10,66 +10,102 @@ using Microsoft.Extensions.Logging;
 
 namespace GoFNOL.Controllers
 {
-    [Authorize]
-    public class FNOLController : Controller
-    {
-        private readonly IFNOLService fnolService;
+	[Authorize]
+	public class FNOLController : Controller
+	{
+		private readonly IFNOLService fnolService;
 
-        private readonly ILogger<FNOLController> logger;
+		private readonly ILogger<FNOLController> logger;
 
-        public FNOLController(IFNOLService fnolService, ILogger<FNOLController> logger)
-        {
-            this.fnolService = fnolService;
-            this.logger = logger;
-        }
+		public FNOLController(IFNOLService fnolService, ILogger<FNOLController> logger)
+		{
+			this.fnolService = fnolService;
+			this.logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			var referer = Request.Headers["Referer"].ToString();
+			var environmentDisplay = GetEnvironmentDisplay(referer, "Local");
+			ViewData["environmentDisplay"] = environmentDisplay;
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-        }
+			return View();
+		}
 
-        [HttpPost("fnol")]
-        public async Task<IActionResult> Create([FromForm] IFormCollection formBody)
-        {
-            try
-            {
-                var request = new FNOLRequest
-                {
-                    ClaimNumber = formBody["claim-number"],
-                    MobileFlowIndicator = formBody["mobile-flow-ind"],
-                    VIN = formBody["vin"],
-                    LossType = formBody["loss-type"],
-                    Deductible = formBody["deductible-waived"] == "on" ? "W" : (string) formBody["deductible"],
-                    Owner = new Party
-                    {
-                        FirstName = formBody["first-name"],
-                        LastName = formBody["last-name"],
-                        PhoneNumber = formBody["phone-number"],
-                        Email = formBody["email"],
-                        Address = new Address
-                        {
-                            PostalCode = $"{(string) formBody["zip-code"]}-{(string) formBody["zip-addon"]}",
-                            State = formBody["state"]
-                        }
-                    }
-                };
-                var sw = Stopwatch.StartNew();
-                var claim = await fnolService.CreateAssignment(request);
-                var workAssignmentId = claim.WorkAssignmentId;
-                TempData["result"] = $"Work Assignment ID: '{workAssignmentId}' added successfully!";
-                TempData["claim"] = $"Claim Number: '{request.ClaimNumber}'.";
-                TempData["responseTime"] = $"It took {(int) sw.Elapsed.TotalSeconds} seconds";
-            }
-            catch (Exception x)
-            {
-                logger.LogError(x, "Error during FNOL process");
-            }
-            return Redirect("/");
-        }
-    }
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		[HttpPost("fnol")]
+		public async Task<IActionResult> Create([FromForm] IFormCollection formBody)
+		{
+			try
+			{
+				var request = new FNOLRequest
+				{
+					ClaimNumber = formBody["claim-number"],
+					MobileFlowIndicator = formBody["mobile-flow-ind"],
+					VIN = formBody["vin"],
+					LossType = formBody["loss-type"],
+					Deductible = formBody["deductible-waived"] == "on" ? "W" : (string)formBody["deductible"],
+					Owner = new Party
+					{
+						FirstName = formBody["first-name"],
+						LastName = formBody["last-name"],
+						PhoneNumber = formBody["phone-number"],
+						Email = formBody["email"],
+						Address = new Address
+						{
+							PostalCode = $"{(string)formBody["zip-code"]}-{(string)formBody["zip-addon"]}",
+							State = formBody["state"]
+						}
+					}
+				};
+				var sw = Stopwatch.StartNew();
+				var claim = await fnolService.CreateAssignment(request);
+				var workAssignmentId = claim.WorkAssignmentId;
+				TempData["result"] = $"Work Assignment ID: '{workAssignmentId}' added successfully!";
+				TempData["claim"] = $"Claim Number: '{request.ClaimNumber}'.";
+				TempData["responseTime"] = $"It took {(int)sw.Elapsed.TotalSeconds} seconds";
+			}
+			catch (Exception x)
+			{
+				logger.LogError(x, "Error during FNOL process");
+			}
+			return Redirect("/");
+		}
+
+		private static string GetEnvironmentDisplay(string referer, string defaultDisplay)
+		{
+			if (string.IsNullOrWhiteSpace(referer))
+			{
+				return defaultDisplay;
+			}
+
+			referer = referer.ToLower();
+
+			if (referer.Contains("prod"))
+			{
+				return "Prod";
+			}
+
+			if (referer.Contains("demo"))
+			{
+				return "Demo";
+			}
+
+			if (referer.Contains("int"))
+			{
+				return "Int";
+			}
+
+			if (referer.Contains("devtest"))
+			{
+				return "DevTest";
+			}
+
+			return referer.Contains("qa") ? "QA" : defaultDisplay;
+		}
+	}
 }

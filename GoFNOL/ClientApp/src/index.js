5e04bb3899868f9eeaedc83d31bf693c2c4a5be5
dmytro.sviridov <dmytro.sviridov@autopoint.com>
2018-10-04 07:32:24 -0600

    Drop serviceWorker.js [#160792852]

--- GoFNOL/ClientApp/src/index.js
+++ GoFNOL/ClientApp/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
@@ -15,5 +14,3 @@ ReactDOM.render(
 	<BrowserRouter basename={baseUrl}>
 		<App />
 	</BrowserRouter>, rootElement)
-
-registerServiceWorker()
